194ed691f13d8f68469d56c8b475a2fee6ecb57e
unknown <mahboob.rahman@hotmail.co.uk>
2019-07-31 12:54:07 +0100

    framework setup

---
+++ Gemfile
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
+
+gem "date"
+gem "rspec"
+gem "selenium"
+gem "selenium-webdriver"
+gem "capybara"
+gem "cucumber"
---
+++ features/support/env.rb
@@ -0,0 +1,36 @@
+require 'capybara/cucumber'
+require 'capybara/dsl'
+require 'rspec'
+require_relative '../lib/bjss_tests_service' #require main page file
+
+# The below line of code now means that the chrome driver is registered before every run.
+# Any query stated at the top level of the env.rb file will be initiated at every cucumber run
+
+Capybara.configure do |config|
+  config.ignore_hidden_elements = false #to ensure that all hidden elements on a page are recorded/available
+  config.match = :prefer_exact #this setting is to ensure Capybara has specific matching rather than fuzzy logic
+
+  if ENV['chrome']
+    Capybara.default_driver = :chrome
+    Capybara.register_driver :chrome do |app|
+      Capybara::Selenium::Driver.new(app, :browser => :chrome)
+    end
+  elsif ENV['firefox']
+    Capybara.default_driver = :firefox
+    Capybara.register_driver :firefox do |app|
+      Capybara::Selenium::Driver.new(app, :browser => :firefox)
+    end
+  elsif ENV['safari']
+    Capybara.default_driver = :safari
+    Capybara.register_driver :safari do |app|
+      Capybara::Selenium::Driver.new(app, :browser => :safari)
+    end
+  else
+    Capybara.default_driver = :chrome
+    Capybara.register_driver :chrome do |app|
+      Capybara::Selenium::Driver.new(app, :browser => :chrome)
+    end
+  end
+end
+
+World(BJSSTests)
